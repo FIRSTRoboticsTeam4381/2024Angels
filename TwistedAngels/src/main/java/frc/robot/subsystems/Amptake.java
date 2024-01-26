04868f876fc9d1f86795cf9b19db6d045b2a57e2
THatMustacheCat <98564568+THatMustacheCat@users.noreply.github.com>
2024-01-25 20:30:46 -0500

    Amptake again because I forgot to hit save

--- TwistedAngels/src/main/java/frc/robot/subsystems/Amptake.java
+++ TwistedAngels/src/main/java/frc/robot/subsystems/Amptake.java
@@ -8,42 +8,51 @@ import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.SparkMaxPosition;
 
 public class Amptake extends SubsystemBase 
 {
   // Creates motors
-  CANSparkFlex pivot1;
-  CANSparkFlex pivot2;
-  CANSparkFlex intake;
-  CANSparkFlex belt;
+  CANSparkMax pivot1;
+  CANSparkMax pivot2;
+  CANSparkMax intake;
+  CANSparkMax belt;
+  DigitalInput breakbeam;
   
   
   public Amptake() 
   {
-    pivot1 = new CANSparkFlex(0, MotorType.kBrushless);
-    pivot2 = new CANSparkFlex(0, MotorType.kBrushless);
-    intake = new CANSparkFlex(0, MotorType.kBrushless);
-    belt = new CANSparkFlex(0, MotorType.kBrushless);
+    pivot1 = new CANSparkMax(0, MotorType.kBrushless);
+    pivot2 = new CANSparkMax(0, MotorType.kBrushless);
+    intake = new CANSparkMax(0, MotorType.kBrushless);
+    belt = new CANSparkMax(0, MotorType.kBrushless);
     pivot2.follow(pivot1);
     pivot2.setInverted(true);
+    breakbeam = new DigitalInput(0);
   }
 
   // Intake on
-  public Command Pickup()
+  public Command pickup()
   {
-    return new ParallelCommandGroup(
-      new InstantCommand(() -> intake.set(1))
-    );
+    //new InstantCommand(() -> intake.set(1))
+    return new FunctionalCommand(
+      () -> intake.set(1),
+      () -> {},
+      (isInterupted) -> intake.set(0),
+      breakbeam::get
+    ,this);
   }
 
   // Spit out a ring out the front 
-  public Command SpitOut()
+  public Command spitOut()
   {
     return new SequentialCommandGroup(
       new InstantCommand(() -> intake.set(-1)),
@@ -53,7 +62,7 @@ public class Amptake extends SubsystemBase
   }
 
   // When the amp pivots are up it is used to place in amp
-  public Command InAmp()
+  public Command inAmp()
   {
     return new SequentialCommandGroup(
       new InstantCommand(() -> intake.set(1)),
@@ -63,19 +72,25 @@ public class Amptake extends SubsystemBase
   }
 
   // Move the note to the shooter
-  public Command PrepShooter()
+  public Command prepShooter()
   {
     return new InstantCommand(() -> belt.set(1));
   }
 
-  // Put the amp bars up
-  public Command AmpPivot()
+  // Move the pivot to a position
+  public Command pivotTo(int position)
+  {
+    return new SparkMaxPosition(pivot1, position, 0, 50, this);
+  }
+
+  // Move pivot
+  public Command ampPivot()
   {
     return new InstantCommand(() -> pivot1.set(1));
   }
   
   // Turn off the intake
-  public Command Off()
+  public Command off()
   {
     return new InstantCommand(() -> intake.set(0));
   }
